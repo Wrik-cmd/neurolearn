e7189c8d1a83fffba7c87c57df4a92fdfb5eb481
Writam <writamsamanta898@gmail.com>
2026-03-31 12:42:52 +0530

    Delete Neurolearn/backend/backend/routes/youtube.routes.js

--- Neurolearn/backend/backend/routes/youtube.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const youtubeController = require('../controller/youtube.controller');
-
-// Optional auth: we can leave it open for demo accounts or apply auth if needed
-router.get('/search', youtubeController.searchVideos);
-
-module.exports = router;
