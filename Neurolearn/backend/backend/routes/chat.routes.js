53b2c930dd9175c38b13043e931a34bb3a649a8d
Writam <writamsamanta898@gmail.com>
2026-03-31 12:42:41 +0530

    Delete Neurolearn/backend/backend/routes/chat.routes.js

--- Neurolearn/backend/backend/routes/chat.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const chatController = require('../controller/chat.controller');
-
-router.post('/ask', chatController.askAssistant);
-
-module.exports = router;
